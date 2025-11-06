f3ea2743291a15e9327958928b95a42853393081
Egemen Günel <egemengunel@gmail.com>
2025-11-06 18:20:43 +0100

    update resume

--- src/components/CVDownload.tsx
+++ src/components/CVDownload.tsx
@@ -1,26 +1,17 @@
 const CVDownload = () => {
-  const CV_IOS_URL = "https://drive.google.com/file/d/1NbJ0KSxO-7gRAEVFfcJFwbUycVh17pMR/view?usp=sharing";
-  const CV_WEB_URL = "https://drive.google.com/file/d/1RVC5S7_SKJzWwz0bYxpQ1SSaKK3CNfAa/view?usp=sharing" 
+  const CV_URL = "https://drive.google.com/file/d/1uYTpi3LnS2Y2sedLcY6NKhL9QkH85I35/view?usp=share_link";
+
 
   return (
     <p className="text-base md:text-lg text-gray-700 mt-4 text-center md:text-left">
-      You can view my resume here: {" "}
+      You can view my resume {" "}
       <a
-        href={CV_IOS_URL}
+        href={CV_URL}
         target="_blank"
         rel="noopener noreferrer"
         className="font-semibold underline decoration-2 underline-offset-4 hover:text-black transition-colors"
       >
-        iOS Developer
-      </a>
-      {" "} | {" "}
-      <a
-        href={CV_WEB_URL}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="font-semibold underline decoration-2 underline-offset-4 hover:text-black transition-colors"
-      >
-        Web Developer
+       here
       </a>
     </p>
   );
